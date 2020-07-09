298f8b9ae07ffdc84dd95f82e14d33cc393c96cb
Nuzat <nuzatchoudhury10@gmail.com>
2020-07-09 13:40:29 +0000

    test

--- calculator.py
+++ calculator.py
@@ -1,5 +1,7 @@
 import definitions
 # The line above will let you separate your concerns by defining functions your calculator might use in a separate file.
-
+print("The program is running")
+# this is a test to see if it pushes to github
 print("Welcome to the Python Calculator\n\nWhat would you like to do?")
 choice = input()
+
