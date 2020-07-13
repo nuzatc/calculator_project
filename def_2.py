fa67dd83a3365363f655fafe087153676879bdec
Nuzat <nuzatchoudhury10@gmail.com>
2020-07-13 13:02:50 +0000

    test one

---
+++ def_2.py
@@ -0,0 +1,40 @@
+# # Feel free to code your definitions here in order to separate your concerns.
+
+# # ask for zip code 
+# # ask for date 
+# # have specific temperature ranges give a specific output 
+# # have specific weather give a specific output 
+
+
+# outside = ["rainy", "sunny"]
+
+# def what_to_wear():
+#     weather = input("What is the weather like today? ")
+#     temperature = int(input("What is the temperature? "))
+#     if temperature <= 50:
+#         if weather == outside[0]:
+#             return ("Wear a thick jacket and take an umbrella!")
+#         else: 
+#             return ("Wear a thick jacket")
+#     if temperature >= 51 and temperature <= 64:
+#         if weather == outside[0]:
+#             return ("Wear a thick hoodie and take an umbrella")
+#         else: 
+#             return ("Wear a thick hoodie")
+#     if temperature >= 65 and temperature <= 70: 
+#         if weather == outside[0]:
+#             return ("Wear a t-shirt and take an umbrella")
+#         else: 
+#             return ("Wear a t-shirt!")
+#     if temperature >= 70 and temperature <= 85: 
+#         if weather == outside[0]:
+#             return ("Wear shorts and take an umbrella")
+#         else: 
+#             return ("Wear shorts")
+#     if temperature > 85: 
+#         if weather == outside[0]:
+#             return ("Wear a bikini, its beach season (but take an umbrella) :)")
+#         else: 
+#             return ("Wear a bikini, its beach season :)")
+
+# print(what_to_wear())
--- definitions.py
+++ definitions.py
@@ -1,1 +1,24 @@
-# Feel free to code your definitions here in order to separate your concerns.
+
+import requests 
+city = input("What is your city?")
+
+api = "http://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=923718dea61d364f34c9b6c3ae119aca"
+
+data = requests.get(api).json()
+
+
+temperature = int((data["main"]["temp"]))
+
+def what_to_wear():
+    if temperature <= 50:
+        return ("Wear a thick jacket")
+    if temperature >= 51 and temperature <= 64:
+        return ("Wear a thick hoodie")
+    if temperature >= 65 and temperature <= 70: 
+        return ("Wear a t-shirt!")
+    if temperature >= 70 and temperature <= 85: 
+        return ("Wear shorts")
+    if temperature > 85: 
+        return ("Wear a bikini, its beach season :)")
+
+print(what_to_wear())
